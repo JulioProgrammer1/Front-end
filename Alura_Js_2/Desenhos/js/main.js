f798f7125c5f0a50395daa0985679f3a055e0e97
JulioProgrammer1 <julioreis1000.pro@gmail.com>
2020-06-02 22:46:49 -0300

    Mais desenhos.

--- Alura_Js_2/Desenhos/js/main.js
+++ Alura_Js_2/Desenhos/js/main.js
@@ -65,3 +65,126 @@ pincel4.moveTo(60, 150);
 pincel4.lineTo(60, 530);
 pincel4.lineTo(450, 530);
 pincel4.fill();
+
+var quadrado = document.querySelector(".canvas5");
+var pincel5 = quadrado.getContext("2d");
+
+function fazUmQuadrado(x,y,cor) {
+    pincel5.fillStyle = cor;
+    pincel5.fillRect(x,y,100,100);
+}
+
+var x = 0;
+var y = 0;
+
+while ( true ) {
+
+    fazUmQuadrado(x, y, "yellow");
+
+    if ( x == 600 && y == 600 ) {
+        break;
+    }
+
+    else if ( x == 600 ) {
+        x = 0;
+        y += 100;
+    }
+    else {
+        x += 100;
+    }
+}
+
+var tela5 = document.querySelector(".canvas6");
+var pincel6 =  tela5.getContext("2d");
+
+pincel6.fillStyle = "lightgrey";
+pincel6.fillRect(0,0,600,400);
+
+function imprimeBolinhas(x,y,) {
+    pincel6.fillStyle = cor;
+    pincel6.beginPath();
+    pincel6.arc(x,y,50,0,2*3.14);
+    pincel6.fill();
+}
+
+var cor = "yellow";
+
+var x = 300;
+var y = 200;
+
+for ( var i = 0; i <= 5; i++ ) {
+
+    imprimeBolinhas(x,y,cor);
+
+    if ( i == 1 ) {
+        y = 100;
+        cor = "blue";
+    }
+    else if ( i == 2 ) {
+        y = 200;
+        x = 200;
+        cor = "red";
+    }
+    else if ( i == 3) {
+        y = 300;
+        x = 300;
+        cor = "orange";
+    }
+    else if ( i == 4 ) {
+        y = 200;
+        x = 400;
+        cor = "purple";
+    }
+}
+
+var grafico = document.querySelector(".canvas7");
+var pincel6 = grafico.getContext("2d");
+
+function fazUmGrafico(x,y,cor,final) {
+    pincel6.fillStyle = cor;
+    pincel6.fillRect(x,y,100,final);
+}
+
+x = 0;
+y = 0;
+var final1  = 100;
+
+for ( var i = 0; i <= 3; i++ ) {
+
+    if ( i == 0 ) {
+        cor = "pink";
+    }
+    
+    if ( i == 1 ) {
+        cor = "yellow";
+    }
+    else if ( i == 2 ) {
+        cor = "blue";
+    }
+
+    fazUmGrafico(x, y, cor, final1);
+
+    final1 =  final1-20;
+}
+
+function esquadro(x,y,x1,y1) {
+
+var tela6 = document.querySelector(".canvas8");
+var pincel7 = tela6.getContext("2d");
+pincel7.fillStyle = "#000";
+pincel7.beginPath();
+pincel7.moveTo(0,0);
+pincel7.lineTo(600,600);
+pincel7.lineTo(0, 600);
+pincel7.fill();
+
+pincel7.fillStyle = "#fff";
+pincel7.beginPath();
+pincel7.moveTo(60, 150);
+pincel7.lineTo(x,y);
+pincel7.lineTo(x1,y1);
+pincel7.fill();
+
+}
+
+esquadro(100 , 500, 300, 400);
